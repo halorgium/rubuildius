8922eef946174af395d5ca01ac78eaa6416a9445
David Whittington <djwhitt@gmail.com>
2008-03-21 14:15:38 -0400

    Added daemonize flag back to launch command in cron.rb

--- cron.rb
+++ cron.rb
@@ -3,5 +3,5 @@
 rubuildius_dir = "~/rubuildius/"
 
 if `ps -A | grep \`cat #{rubuildius_dir}matzbot.pid\` | wc`.to_i.zero?
-  system("cd #{rubuildius_dir}; rm matzbot.pid; ./launch.rb -u rubuildius_xxx -n rubuildius_xxx -m rubuildius_xxx -c rubuildius_test")
+  system("cd #{rubuildius_dir}; rm matzbot.pid; ./launch.rb -d -u rubuildius_xxx -n rubuildius_xxx -m rubuildius_xxx -c rubuildius_test")
 end
