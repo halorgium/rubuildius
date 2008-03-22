6678db211ea7547e1ce9d4ea3d20cad5f5303a34
David Whittington <djwhitt@gmail.com>
2008-03-22 17:29:11 -0400

    Removed cron.rb (specs need terminal, so we can't run from cron)

--- cron.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-rubuildius_dir = "~/rubuildius/"
-
-if `ps -A | grep \`cat #{rubuildius_dir}matzbot.pid\` | wc`.to_i.zero?
-  system("cd #{rubuildius_dir}; rm matzbot.pid; ./launch.rb -d -u rubuildius_xxx -n rubuildius_xxx -m rubuildius_xxx -c rubuildius_test")
-end
