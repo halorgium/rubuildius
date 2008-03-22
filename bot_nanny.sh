a5dad52c40870af10c06fa49d4d311ab8ed6d284
David Whittington <djwhitt@gmail.com>
2008-03-22 02:16:31 -0400

    Switching back to sub-processes nanny, will mod specs to handle it

--- bot_nanny.sh
+++ bot_nanny.sh
@@ -5,11 +5,8 @@ IRC_NICK=rubuildius_xxx
 IRC_NAME=rubuildius_xxx
 IRC_CHANNEL=rubuildius_test
 
-cd $HOME/rubuildius/
-
-bot_status=`ps -A | grep \`cat matzbot.pid\` | wc -l`
-
-if [ $bot_status = '0' ]; then
-    rm matzbot.pid
-    exec ruby launch.rb -u $IRC_USER -n $IRC_NICK -m $IRC_NAME -c $IRC_CHANNEL
-fi
+while true;
+do
+    ./launch.rb -u $IRC_USER -n $IRC_NICK -m $IRC_NAME -c $IRC_CHANNEL
+    sleep 30
+done
